b16094d59b1af7fd591b9f3d3397acefedd0fb04
w <36293421+ppond454@users.noreply.github.com>
2021-11-02 18:06:47 +0700

    Add files via upload

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,43 @@
 import type { NextPage } from "next"
-
+import { GetStaticProps } from "next"
 import Nav from "../components/nav"
 import Bio from "../components/bio"
 import Intro from "../components/intro"
 import Footer from "../components/footer"
 import Skill from "../components/skill"
+import Project from "../components/project"
 
-const Home: NextPage = () => {
+
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const skill = {
+    lang: ["JavaScript", "TypeScript", "CSS", "HTML", "C", "Python"],
+    fwork: ["ReactJS","NextJS","VueJS","Flask","OpenCV"],
+  }
+
+  return {
+    props: {
+      skill,
+    },
+  }
+}
+
+interface Props {
+  skill: {
+    lang: string[],
+    fwork: string[],
+  }
+}
+
+const Home = ({ skill }: Props) => {
   return (
     <>
       <Nav />
-      <Intro/>
+      <Intro />
       <Bio />
-      <Skill/>
-      <Footer/>
+      <Skill skill={skill} />
+      <Project/>
+      <Footer />
     </>
   )
 }
