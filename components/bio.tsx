b4ef6092ff169c4613fbac2f8a34ef7bb5a7eec6
w <36293421+ppond454@users.noreply.github.com>
2021-11-05 02:01:37 +0700

    Add files via upload

--- components/bio.tsx
+++ components/bio.tsx
@@ -1,14 +1,6 @@
-import {
-  Container,
-  Text,
-  Heading,
-  Box,
-  Divider,
-  Stack,
-} from "@chakra-ui/react"
+import { Container, Text, Heading, Box, Divider, Stack } from "@chakra-ui/react"
 import React from "react"
 import Motions from "../containers/motions/motions"
-
 
 interface Props {
   detail: {
@@ -20,19 +12,31 @@ interface Props {
 
 const Bio = ({ detail }: Props) => {
   return (
-    <Container mb="8" mt="8" >
-      <Heading size="lg" variant="section-title" >Education</Heading>
+    <Container mb="8" mt="8">
+      <Heading size="lg" variant="section-title">
+        Education
+      </Heading>
       {detail.map((val, i) => {
         return (
-          <Stack direction="row" h="120px" p={4} mt="4" >
-            <Text fontSize={{base:"15px" ,md:"16px"}} >{val.years}</Text>
-            <Divider  orientation="vertical" bg="teal.200" w="1" />
+          <Stack
+            direction="row"
+            h="120px"
+            p={4}
+            mt="4"
+            display={{ base: "ruby-text-container", md: "inline-flex" }}
+            
+          >
+            <Text fontSize={{ base: "12px", md: "14px" }}>{val.years}</Text>
+            <Divider orientation="vertical"  bg="teal.200" w={{base:"0.5" , md:"1"}} />
             <Box>
-            <Text fontSize={{base:"15px" ,md:"16px"}}>{val.enducation}</Text>
-            <Text
-            color="red.200" 
-            fontSize={{base:"12px" ,md:"13px"}}>{val.degree}</Text>
+              <Text fontSize={{ base: "14px", md: "16px" }}>
+                {val.enducation}
+              </Text>
+              <Text color="red.200" fontSize={{ base: "11px", md: "13px" }}>
+                {val.degree}
+              </Text>
             </Box>
+          
           </Stack>
         )
       })}
