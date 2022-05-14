208ffdc6c9d02275e5bf35b8a554ec5ad3219642
Ppond454 <ppond.454@gmail.com>
2022-05-15 01:29:57 +0700

    15/05/22

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@chakra-ui/react"
+import { Center } from "@chakra-ui/react"
 
 import React from "react"
 
@@ -6,9 +6,9 @@ interface Props {}
 
 const Footer = (props: Props) => {
   return (
-    <Box align="center" opacity={0.4} fontSize="sm"  mt="10" p="5" h="100">
+    <Center opacity={0.4} fontSize="sm"  mt="10" p="5" h="100">
       &copy; {new Date().getFullYear()} Siritep Tongduang. All Rights Reserved.
-    </Box>
+    </Center>
   )
 }
 
